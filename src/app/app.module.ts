db325e7721c5d75c88bb01519b83d5ec868fd010
FahmiDeveloper <coursangularfahmi@gmail.com>
2020-12-28 13:21:55 +0100

    Protecting the Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
 
 @NgModule({
   declarations: [
@@ -47,16 +48,19 @@ import { AuthService } from './auth.service';
       { path:'', component:HomeComponent},
       { path:'products', component:ProductsComponent},
       { path:'shopping-cart', component:ShoppingCartComponent},
-      { path:'check-out', component:CheckOutComponent},
-      { path:'order-success', component:OrderSuccessComponent},
       { path:'login', component:LoginComponent},
-      { path:'admin/products', component:AdminProductsComponent},
-      { path:'admin/orders', component:AdminOrdersComponent},
-      { path:'my/orders', component:MyOrdersComponent}
+
+      { path:'check-out', component:CheckOutComponent, canActivate: [AuthGuard]},
+      { path:'order-success', component:OrderSuccessComponent, canActivate: [AuthGuard]},
+      { path:'my/orders', component:MyOrdersComponent, canActivate: [AuthGuard]},
+
+      { path:'admin/products', component:AdminProductsComponent, canActivate: [AuthGuard]},
+      { path:'admin/orders', component:AdminOrdersComponent, canActivate: [AuthGuard]}
     ])
   ],
   providers: [
-    AuthService
+    AuthService,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
