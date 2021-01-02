2d770ae9413e134ec00213c1868707be39f06280
FahmiDeveloper <coursangularfahmi@gmail.com>
2021-01-02 10:59:56 +0100

    Displaying the List of Products

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
+  listProducts: AngularFireList<any>;
+
   constructor(private db: AngularFireDatabase) { }
 
   create(product) {
     return this.db.list('/products').push(product);
+  }
+
+  getAll() {
+    this.listProducts = this.db.list('/products');
+    return this.listProducts
+     .snapshotChanges()
+     .pipe(map(changes => changes
+     .map(c => ({ key: c.payload.key, ...c.payload.val() }))));
   }
 }
