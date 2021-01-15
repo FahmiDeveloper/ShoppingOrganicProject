099eb0d096a07337e3a8be70acf49aa49fbfad61
FahmiDeveloper <coursangularfahmi@gmail.com>
2021-01-15 17:41:00 +0100

    Refactoring - Extracting ProductQuantityComponents

--- src/app/product-quantity/product-quantity.component.ts
+++ src/app/product-quantity/product-quantity.component.ts
@@ -1,25 +1,28 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Product } from '../models/product';
-import { ShoppingCart } from '../models/shopping-cart';
 import { ShoppingCartService } from '../shopping-cart.service';
 
 @Component({
-  selector: 'product-card',
-  templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss']
+  selector: 'product-quantity',
+  templateUrl: './product-quantity.component.html',
+  styleUrls: ['./product-quantity.component.scss']
 })
-export class ProductCardComponent {
+export class ProductQuantityComponent {
 
   @Input('product') product: Product;
-
-  @Input('show-actions') showActions = true;
-
-  @Input('shopping-cart') shoppingCart: ShoppingCart;
+  @Input('shopping-cart') shoppingCart;
 
   constructor(private cartService: ShoppingCartService) { }
 
   addToCart() {
     this.cartService.addToCart(this.product);
   }
+
+  removeFromCart() {
+    this.cartService.removeFromCart(this.product);
+  }
+
+  
+  
 
 }
